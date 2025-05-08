833e0304f2198c7d3014fd2e94225a7014e6becb
ivan-hryshko <giver313@gmail.com>
2025-05-08 11:16:24 +0300

    feat: basic project setup

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+// import routes  from './routes/index'
+
+// import postgresSource from './config/app-data-source'
+
+
+const app = express()
+
+// postgresSource
+//   .initialize()
+//   .then(() => {
+//       console.log("Data Source has been initialized!")
+//   })
+//   .catch((err) => {
+//       console.error("Error during Data Source initialization:", err)
+//   })
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// app.use('/api/v1/', routes)
+
+
+export default app
